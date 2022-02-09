1da8e4bd22f52a2a9d65e4d55ee2447de915a449
Penn Wilbert <pwilbert@gmail.com>
2022-02-08 21:16:23 -0500

    changed styling

--- interface.js
+++ interface.js
@@ -5,35 +5,35 @@ async function run() {
     await init();
 
     const submit_button = document.getElementById('submit');
-    let input_size = "75px";
-    document.getElementById('first_placed').style.height=input_size;
-    document.getElementById('second_placed').style.height=input_size;
-    document.getElementById('third_placed').style.height=input_size;
-    document.getElementById('fourth_placed').style.height=input_size;
-    document.getElementById('fifth_placed').style.height=input_size;
-    document.getElementById('found_letters').style.height="50px";
+    // let input_size = "75px";
+    // document.getElementById('first_placed').style.height=input_size;
+    // document.getElementById('second_placed').style.height=input_size;
+    // document.getElementById('third_placed').style.height=input_size;
+    // document.getElementById('fourth_placed').style.height=input_size;
+    // document.getElementById('fifth_placed').style.height=input_size;
+    // document.getElementById('found_letters').style.height="50px";
 
-    document.getElementById('first_placed').style.width=input_size;
-    document.getElementById('second_placed').style.width=input_size;
-    document.getElementById('third_placed').style.width=input_size;
-    document.getElementById('fourth_placed').style.width=input_size;
-    document.getElementById('fifth_placed').style.width=input_size;
-    // document.getElementById('found_letters').style.width=input_size;
+    // document.getElementById('first_placed').style.width=input_size;
+    // document.getElementById('second_placed').style.width=input_size;
+    // document.getElementById('third_placed').style.width=input_size;
+    // document.getElementById('fourth_placed').style.width=input_size;
+    // document.getElementById('fifth_placed').style.width=input_size;
+    // // document.getElementById('found_letters').style.width=input_size;
 
-    let font_size = "x-large"
-    document.getElementById('first_placed').style.fontSize=font_size;
-    document.getElementById('second_placed').style.fontSize=font_size;
-    document.getElementById('third_placed').style.fontSize=font_size;
-    document.getElementById('fourth_placed').style.fontSize=font_size;
-    document.getElementById('fifth_placed').style.fontSize=font_size;
-    document.getElementById('found_letters').style.fontSize=font_size;
+    // let font_size = "x-large"
+    // document.getElementById('first_placed').style.fontSize=font_size;
+    // document.getElementById('second_placed').style.fontSize=font_size;
+    // document.getElementById('third_placed').style.fontSize=font_size;
+    // document.getElementById('fourth_placed').style.fontSize=font_size;
+    // document.getElementById('fifth_placed').style.fontSize=font_size;
+    // document.getElementById('found_letters').style.fontSize=font_size;
 
-    let font_position = "center"
-    document.getElementById('first_placed').style.textAlign=font_position;
-    document.getElementById('second_placed').style.textAlign=font_position;
-    document.getElementById('third_placed').style.textAlign=font_position;
-    document.getElementById('fourth_placed').style.textAlign=font_position;
-    document.getElementById('fifth_placed').style.textAlign=font_position;
+    // let font_position = "center"
+    // document.getElementById('first_placed').style.textAlign=font_position;
+    // document.getElementById('second_placed').style.textAlign=font_position;
+    // document.getElementById('third_placed').style.textAlign=font_position;
+    // document.getElementById('fourth_placed').style.textAlign=font_position;
+    // document.getElementById('fifth_placed').style.textAlign=font_position;
 
     submit_button.addEventListener('click', () => {
         var first = (document.getElementById('first_placed').value != "") ? document.getElementById('first_placed').value : '\0';
