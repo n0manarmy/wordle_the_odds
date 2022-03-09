23cc760de39c53ada95f378e021a2eafdb6c0f94
Penn Wilbert <pwilbert@gmail.com>
2022-03-08 21:40:10 -0500

    gitignore words.rs for sake of spoilers

--- build.rs
+++ build.rs
@@ -30,7 +30,7 @@ fn build_list(word_list: Vec<WordleWord>) -> String {
 }
 
 fn main() {
-    let file = std::fs::File::open("words_test.json").unwrap();
+    let file = std::fs::File::open("list_utils/words_test.json").unwrap();
     let file = std::io::BufReader::new(file);
     let word_lists: WordList = serde_json::from_reader(file).expect("error parsing json file");
     
@@ -76,20 +76,3 @@ pub static ANSWER_LIST: [WordleWord; {}] =
     ).unwrap();
     println!("carg:rerun-if-changed=build.rs");
 }
-
-// fn cleanup_input() {
-//     let contents = fs::read_to_string("temp").unwrap();
-//     let split: Vec<&str> = contents.split('\n').collect();
-//     for s in split {
-//         println!("{}", s);
-//         write_to_file(&["\"", &s, "\","].concat(), "out");
-//     }
-
-// }
-
-// fn write_to_file(line: &str, path: &str) {
-//     match std::fs::OpenOptions::new().read(true).create(true).append(true).open(path) {
-//         Ok(mut s) => s.write_all([line, "\n"].concat().as_bytes()).expect("Error writing to file"), 
-//         Err(why) => panic!("{}", why),
-//     }
-// }
--- src/words.rs
+++ src/words.rs
@@ -26,14 +26,12 @@ pub static TEST_ANSWER_LIST: [WordleWord; 5] =
     }
 ];
 
-pub static ANSWER_LIST: [WordleWord; 7] = 
+pub static ANSWER_LIST: [WordleWord; 5] = 
 [
-	WordleWord { word: "cigar", zipf_dist: 3.57},
-	WordleWord { word: "rebut", zipf_dist: 2.6},
-	WordleWord { word: "sissy", zipf_dist: 3.23},
-	WordleWord { word: "humph", zipf_dist: 2.18},
-	WordleWord { word: "awake", zipf_dist: 4.14},
-	WordleWord { word: "blush", zipf_dist: 3.56},
-	WordleWord { word: "focal", zipf_dist: 3.67},
+	WordleWord { word: "aback", zipf_dist: 3.57},
+	WordleWord { word: "abase", zipf_dist: 2.6},
+	WordleWord { word: "abate", zipf_dist: 3.23},
+	WordleWord { word: "abbey", zipf_dist: 2.18},
+	WordleWord { word: "abbot", zipf_dist: 4.14},
 
 ];
